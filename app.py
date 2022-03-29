76f303601ba07da9e35e1035e25517133dfaf47c
Anurag Thakur <anut2405@gmail.com>
2022-03-30 01:10:04 +0530

    converted types and almost completed

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
 from flask import Flask, redirect, render_template, request
 import pandas as pd
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+import xgboost
 
 app = Flask(__name__)
+model = pickle.load(open('proj.pkl', 'rb'))
 
 @app.route("/", methods = ['GET','POST'])
 def hello_world():
     if request.method == 'POST':
-        budget = request.form['budget']
-        populatiry = request.form['populatiry']
-        runtime = request.form['runtime']
-        release_quarter = request.form['release_quarter']
-        release_month = request.form['release_month']
-        release_day = request.form['release_day']
-        release_year = request.form['release_year']
-        release_dayofweek = request.form['release_dayofweek']
-        is_released = request.form['is_released']
-        has_homepage = request.form['has_homepage']
-        has_a_tagline = request.form['has_a_tagline']
-        originallang_en = request.form['originallang_en']
-        has_collection = request.form['has_collection']
-        num_genres = request.form['num_genres']
-        print(has_a_tagline)
-        print(type(has_a_tagline))
+        budget = float(request.form['budget'])
+        populatiry = float(request.form['populatiry'])
+        runtime = float(request.form['runtime'])
+        release_month = int(request.form['release_month'])
+        release_day = int(request.form['release_day'])
+        release_year = int(request.form['release_year'])
+        release_dayofweek = int(request.form['release_dayofweek'])
+        release_quarter = int(request.form['release_quarter'])
+        is_released = int(request.form['is_released'])
+        has_homepage = int(request.form['has_homepage'])
+        has_a_tagline = int(request.form['has_a_tagline'])
+        originallang_en = int(request.form['originallang_en'])
+        has_collection = int(request.form['has_collection'])
+        num_genres = int(request.form['num_genres'])
         # data = pd.DataFrame({"budget":budget,"populatiry":populatiry})
         # print(data["budget"])
-    return render_template('index.html')
+        prediction=model.predict([[]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry prediction not available")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+    # return render_template('index.html')
 
 
 if __name__ == "__main__":
